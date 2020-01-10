c743e3d27c135c8ca5588605c9234a4fee01b73d
Thaisa <thaisaelvas@gmail.com>
2020-01-10 01:37:35 -0300

    Quarto exercício do Módulo 1 de JS

---
+++ Ex4Mod1.js
@@ -0,0 +1,26 @@
+//Escreva uma função que dado um total de anos de estudo retorna 
+//o quão experiente o usuário é.
+
+// De 0-1 ano: Iniciante
+// De 1-3 anos: Intermediário
+// De 3-6 anos: Avançado
+// De 7 acima: Jedi Master
+
+function experiencia(anos) {
+    if (anos <= 1) {
+        return 'Iniciante';
+    }
+    else if (anos > 1 && anos <= 3) {
+        return 'Intermediário';
+    }
+    else if (anos > 3 && anos <= 6) {
+        return 'Avançado';
+    }
+    else {
+        return 'Jedi Master';
+    }
+}
+
+let anosEstudo = 0;
+const resultado = experiencia(anosEstudo);
+console.log(resultado);
