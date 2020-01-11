710dec8fbee313734838016c20644fecf62f57af
Thaisa <thaisaelvas@gmail.com>
2020-01-11 14:17:50 -0300

    Correção do Primeiro exercício do Módulo 1 de JS

--- Ex1Mod1.js
+++ Ex1Mod1.js
@@ -2,7 +2,7 @@
 //Crie uma função que dado o objeto retorne o seguinte conteúdo: 
 //O usuário mora em São Paulo / SP, no bairro Centro, na rua "Rua dos Pinheiros" com nº 1293.
 
-var endereco = {
+var enderecoMaria = {
     rua: "Rua dos Pinheiros",
     numero: 1293,
     bairro: "Centro",
@@ -12,8 +12,8 @@ var endereco = {
 
 function mostrarEndereco (endereco) {
     return ("O usuário mora em " + endereco.cidade + " / " + endereco.uf + ", " + 
-    "na rua " endereco.rua + " com nº " + endereco.numero + ".");
+    "na rua " + endereco.rua + " com nº " + endereco.numero + ".");
 }
 
-const resultado = mostrarEndereco(endereco);
+const resultado = mostrarEndereco(enderecoMaria);
 console.log(resultado);
